f2dd0e7863aa0617cf1133b9a42466f99f2c4e25
John Askew <john.askew@deptagency.com>
2022-07-18 16:37:31 -0400

    Create react application for DXP

---
+++ src/dept-dxp-web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="relative flex min-h-screen flex-col overflow-hidden bg-gray-50 py-6 sm:py-12">
+      <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 w-11/12 mx-auto">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
